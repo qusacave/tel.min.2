e24369e0a02970fe5c1e187bb6ba4c9fc28d8f11
q <q@numinfo-no-MacBook-Pro-2.local>
2014-09-23 16:59:48 +0900

    tel.minとしての開発をやめ，個人的にtel.min.2として開発を継続する．

--- js/screen.js
+++ js/screen.js
@@ -10,8 +10,8 @@ lx2Rgb = function( lx ) {
         return 0;
     }
     else {
-	    rgbValue = 255 - (lx/1120)*255;
-	    return Math.round(rgbValue);
+	rgbValue = 255 - (lx/1120)*255;
+	return Math.round(rgbValue);
     }
 };
 
@@ -42,14 +42,94 @@ onMuteButton = function() {
     // 　ミュートしてなかったらミュートする
     if( nowMute ) {
         setMute( false );
-	    nowMute = false;;
-        document.getElementById("imgButton").src = "resource/unmute.png";
+	nowMute = false;;
+        document.getElementById("imgButton").src = "rsrc/unmute.png";
     }
 
     //  ミュート状態だったらミュートにする
     else {
-	    setMute( true );
-	    nowMute = true;
-        document.getElementById("imgButton").src = "resource/mute.png";
+	setMute( true );
+	nowMute = true;
+        document.getElementById("imgButton").src = "rsrc/mute.png";
     }
 };
+
+//------------------------------------------------------------------------------
+//  キーを並べる
+//------------------------------------------------------------------------------
+var onclickPos = [0, 0];
+var baseWidth;
+updateWindow = function(){
+    var wd = window.innerWidth;
+    var ht = window.innerHeight - 80;
+    (wd<ht) ? (baseWidth=wd) : (baseWidth=ht);
+    console.log(baseWidth);
+    keys();
+};
+
+//  ボタンを4つずつ配置する
+keys = function(){
+    var nKeys = Math.ceil( key.length/4 );
+    var keyWidth = baseWidth/4;
+
+    var cnv = document.getElementById("keys");
+    cnv.setAttribute( "width", baseWidth );
+    cnv.setAttribute( "height", baseWidth*(nKeys/4) );
+    var ctx=cnv.getContext("2d");
+    ctx.textAlign = "center";
+
+    for(i=0; i<nKeys; i++){
+        for(j=0; j<4; j++){
+            ctx.beginPath();
+            var x = keyWidth*0.5 + keyWidth*j;
+            var y = keyWidth*0.5 + keyWidth*i;
+            ctx.arc(x, y, keyWidth*0.45, 0, Math.PI*2, true);
+            if(onclickPos[0]==i && onclickPos[1]==j){
+                grad = ctx.createRadialGradient(x, y, keyWidth*0.38, x, y, keyWidth*0.44);
+                grad.addColorStop(0, "#f7f7f7");
+                grad.addColorStop(0.5, "#e0e0e0");
+                grad.addColorStop(0.75, "#a0a0a0");
+                grad.addColorStop(1, "rgba(128, 128, 128, 0)");
+                ctx.fillStyle = grad;
+                ctx.fill();
+                ctx.fillStyle = "#c0c0c0";
+            }
+            else{
+                grad = ctx.createRadialGradient(x, y, keyWidth*0.38, x, y, keyWidth*0.44);
+                grad.addColorStop(0, "#c7c7c7");
+                grad.addColorStop(0.5, "#b0b0b0");
+                grad.addColorStop(0.75, "#707070");
+                grad.addColorStop(1, "rgba(128, 128, 128, 0)");
+                ctx.fillStyle = grad;
+                ctx.fill();
+                ctx.fillStyle = "#909090";
+            }
+            ctx.fillText(key[4*i+j], x, y+3);
+        }
+    }
+
+//  ここからクリックしたときの処理
+    cnv.onclick = ( function(e){
+        var rect = e.target.getBoundingClientRect();
+        var mouseX = e.clientX - rect.left;
+        var mouseY = e.clientY - rect.top;
+        //console.log(mouseX, mouseY);
+
+        for(i=0; i<nKeys; i++){
+            for(j=0; j<4; j++){
+                if(j*keyWidth<=mouseX && mouseX<(j+1)*keyWidth){
+                    if(i*keyWidth<=mouseY && mouseY<(i+1)*keyWidth){
+                        changeKey( 4*i+j );
+                        onclickPos = [i, j];
+                        updateWindow();
+                    }
+                }
+            }
+        }
+    } );
+};
+
+//  起動時と画面幅が変わったら再描画する
+onload = updateWindow;
+window.onresize = updateWindow;
+
--- js/sound.js
+++ js/sound.js
@@ -28,8 +28,33 @@ var pitch = [
     880,                        //  ラ
     880 * Math.pow( 1.06, 1 ),  //  ラ#
     880 * Math.pow( 1.06, 2 ),  //  シ
-    880 * Math.pow( 1.06, 3 )
+    880 * Math.pow( 1.06, 3 ),
+    880 * Math.pow( 1.06, 4 ),  //  ド#
+    880 * Math.pow( 1.06, 5 ),  //  レ
+    880 * Math.pow( 1.06, 6 ),  //  レ#
+    880 * Math.pow( 1.06, 7 ),  //  ミ
+    880 * Math.pow( 1.06, 8 ),  //  ファ
+    880 * Math.pow( 1.06, 9 ),  //  ファ#
+    880 * Math.pow( 1.06,10 ),  //  ソ
+    880 * Math.pow( 1.06,11 ),  //  ソ#
+    1760,                        //  ラ
+    1760 * Math.pow( 1.06, 1 ),  //  ラ#
+    1760 * Math.pow( 1.06, 2 ),  //  シ
+    1760 * Math.pow( 1.06, 3 )
 ];
+
+var key = [
+    "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+];
+
+var diatonic = [
+    0, 2, 4, 5, 7, 9, 11,
+    12, 14, 16, 17, 19, 21, 23,
+    24
+];
+
+var keyScale = 0;
+var lx = 0;
 
 var gAudioContext   = new AudioContext();
 var gOscillatorNode = gAudioContext.createOscillator();
@@ -41,30 +66,44 @@ gGainNode.connect( gAudioContext.destination );
 gGainNode.gain.value = 0;
 
 gOscillatorNode.type = 'sawtooth';
-gOscillatorNode.frequency.value = 440;
+gOscillatorNode.frequency.value = 220;
 gOscillatorNode.start(0);
 
+changeInterval = function(){
+            var freqs = keyScale + diatonic[Math.floor( clamp( lx ) / 67 )];
+            console.log(freqs);
+            gOscillatorNode.frequency.value = pitch[ freqs ];
+};
 
 //------------------------------------------------------------------------------
-//  照度を 20 から 1100 の間におさめる
+//  照度を 0 から 1000 の間におさめる
 //------------------------------------------------------------------------------
 clamp = function( lx ){
-	if( lx > 1120 ) return 1120;
+	if( lx > 1000 ) return 1000;
 	else if( lx < 0 ) return 0;
 	else return lx;
 };
-
 
 //------------------------------------------------------------------------------
 //  照度センサーの値が変わると呼ばれる関数
 //
 //  lx 明るさ (単位ルクス)
 //------------------------------------------------------------------------------
-changeSound = function( lx ) {
-	var freqs = Math.floor( clamp( lx ) / 80 );
-	gOscillatorNode.frequency.value = pitch[ freqs ];
+changeSound = function( luminous ) {
+            lx = luminous;
+            changeInterval();
 };
 
+//------------------------------------------------------------------------------
+//  画面がクリックされたら呼ばれる
+//------------------------------------------------------------------------------
+changeKey = function( key ){
+    //console.log(key);
+    if(key<pitch.length){
+        keyScale = key;
+        changeInterval();
+    }
+};
 
 //------------------------------------------------------------------------------
 //  ミュートする
